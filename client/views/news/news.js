9b9bdd327e5ff2156fda6ae2c03a75ed4721afd8
nikhilpi <nikhilpai2016@u.northwestern.edu>
2015-08-21 00:23:36 -0700

    better mobile scroll

--- client/views/news/news.js
+++ client/views/news/news.js
@@ -12,6 +12,20 @@ Template['news'].onRendered(function() {
     console.log(stream)
     Session.set('stream', stream);
   });
+
+  var body = document.body,
+    timer;
+
+  window.addEventListener('scroll', function() {
+    clearTimeout(timer);
+    if(!body.classList.contains('disable-hover')) {
+      body.classList.add('disable-hover')
+    }
+    
+    timer = setTimeout(function(){
+      body.classList.remove('disable-hover')
+    },500);
+  }, false);
 });
 
 Template['fb_block'].helpers({
