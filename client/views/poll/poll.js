0d080190e254c3de96913ce8eaf033ab6a4231df
David Latimore, pt. 2 <davidlatimore2019@u.northwestern.edu>
2016-05-24 00:56:43 -0500

    pushing

---
+++ client/views/poll/poll.js
@@ -0,0 +1,36 @@
+Template['news'].helpers({
+  'stream': function() {
+    return Session.get('stream');
+  },
+});
+
+Template['news'].events({
+});
+
+Template['news'].onRendered(function() {
+  Meteor.call('getSocialStream', function(error, stream) {
+    Session.set('stream', stream);
+  });
+
+  var body = document.body,
+    timer;
+
+  window.addEventListener('scroll', function() {
+    clearTimeout(timer);
+    if(!body.classList.contains('disable-hover')) {
+      body.classList.add('disable-hover')
+    }
+    
+    timer = setTimeout(function(){
+      body.classList.remove('disable-hover')
+    },200);
+  }, false);
+});
+
+Template['fb_block'].helpers({
+  'fbImage': function(id) {
+    Meteor.call('getFB', id, function(err, data) {
+      $('div[data-fb-id="'+id+'"]').attr("style", 'background-image: url("'+data.source+'");');         
+    });
+  }
+});
